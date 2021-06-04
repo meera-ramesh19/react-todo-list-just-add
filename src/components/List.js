26e5d8a310085cff93558326788c26f349137b40
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-04 09:44:20 -0400

    commenting for understanding

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,12 @@
+  
+// import todo element from todo file
 import Todo from './Todo'
 
+// creates list of todo items to be exported, takes object containing todos as parameter
 const List = ({ todos }) => {
+    // will return the following
     return (
+        // each todo in the todos object is passed to the Todo component file to create a component for each
         <div>
           {todos.map((todo) => 
             <Todo key={todo.id} todo={todo} />
@@ -10,5 +15,5 @@ const List = ({ todos }) => {
     )
 }
 
+// exports list component that contains however many Todo components
 export default List
-
