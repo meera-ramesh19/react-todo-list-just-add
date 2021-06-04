67559376244698916dda8fbff9e548fb51942594
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-04 09:45:03 -0400

    commenting for understanding

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,13 @@
-
+// creates Todo that will be exported
 const Todo = ({ todo }) => {
+    // will return the following html
     return (
+        // a div with a h2 that is the given the task
         <div>
             <h2>{todo.item}</h2>
         </div>
     )
 }
 
+// export single todo component
 export default Todo
-
