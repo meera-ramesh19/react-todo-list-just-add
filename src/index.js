5e41f1b72081a25c4c3c0d6dc1b36ed3410eef99
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-04 08:39:43 -0400

    commenting for understanding

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 //importing all the necessary files and dependencies
-
+  
+// imports react component functionality from react
 import React from 'react';
+// imports react DOM interaction functionality from react
 import ReactDOM from 'react-dom';
+// imports index styles
 import './index.css';
+// imports app component from app file
 import App from './App';
+// imports reportWebVitals from file. Allows site to cache resources
 import reportWebVitals from './reportWebVitals';
 
 //ReactDOM renders stuff to the DOM
+// renders the following
 ReactDOM.render(
   //StrictMode is a tool for highlighting potential problems in an application.
   //StrictMode does not render any visible UI.
@@ -19,11 +25,14 @@ ReactDOM.render(
 //Applications built with just React usually have a single root DOM node. 
  // If you are integrating React into an existing app, you may have as many isolated root
   //DOM nodes as you like.
-  
+  // will render app component in #root element of index.html
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+// this caches resources so the page will still be viewable on slow connections
 reportWebVitals();
