8236524ea644f3e66e290a7d3f9becf395840bdb
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-04 09:41:48 -0400

    commenting for understanding

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,9 @@
+// imports useState from react
+
 import { useState } from 'react'
 // We are adding the todos when the user inputs the data
 
+// create AddTodo app that will be exported, takes new todo info from app.js function addTodo
 const AddTodo = ({ onAdd }) => { 
     //array destructuring 
     const [item, setItem] = useState('')
@@ -13,8 +16,10 @@ const AddTodo = ({ onAdd }) => {
         setItem('')
     }
 
+    // will return the following
     return (
         //form used to accept input from the user
+         // a form that allows user to submit a new todo item
         <form onSubmit={onSubmit}>
             <label>Add Todo:</label>
             <input 
